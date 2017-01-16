5bae8fae0de5d60bad6eff24558a97fb1bdec2f3
Jake <jacobo.strm@gmail.com>
2017-01-16 22:48:49 +0100

    Finished the voting results parsing.

--- scbminer/voting_highscores/services.py
+++ scbminer/voting_highscores/services.py
@@ -1,18 +1,52 @@
 import json
 import requests
 
+# URLs should be put in some configuration
 VOTING_RESULTS_URL = 'http://api.scb.se/OV0104/v1/doris/sv/ssd/START/ME/ME0104/ME0104D/ME0104T4'
+VALUE_COLUMN = 'c'
 
 
 def get_region_highscores():
     voting_result_variables = retrieve_scb_voting_result_variables()
     region_code_map = create_region_code_map(voting_result_variables)
     riksdag_code = extract_riksdag_code(voting_result_variables)
-    scb_region_data = retrieve_scb_region_voting_data_by_type(riksdag_code)
-    return region_code_map
+    scb_region_data = retrieve_scb_voting_data_by_type(riksdag_code)
+    voting_results = extract_voting_results_by_year(scb_region_data,
+                                                    region_code_map)
+    return voting_results
+
+
+def extract_voting_results_by_year(scb_region_data, region_code_map):
+    """ Extracts and returns a dictionary with year as key and
+        the area with highest turnout rate as value """
+    data_keys = extract_data_keys(scb_region_data)
+    results = {}
+    year_index = data_keys['Tid']
+    region_index = data_keys['Region']
+
+    for data in scb_region_data['data']:
+        year = data['key'][year_index]
+        region_name = region_code_map[data['key'][region_index]]
+        value = data['values'][0]
+        results.setdefault(year, []).append((region_name, value))
+
+    for years in results.keys():
+        results[years].sort(key=lambda result: result[1], reverse=True)
+        results[years] = results[years][0]
+
+    return results
+
+
+def extract_data_keys(scb_region_data):
+    """ This map is used to keep track of what index the data has """
+    columns = list(enumerate(scb_region_data['columns']))
+    data_keys = [(column['code'], i) for (i, column) in columns
+                 if column['type'] != VALUE_COLUMN]
+    return dict(data_keys)
 
 
 def create_region_code_map(voting_result_variables):
+    """ This map is used to identify what codes each region has """
     value_texts = extract_value_texts(voting_result_variables)
     values = extract_values(voting_result_variables)
     return dict(zip(values, value_texts))
@@ -20,7 +54,9 @@ def create_region_code_map(voting_result_variables):
 
 def extract_riksdag_code(scb_response):
     codes = extract_variable_data(scb_response, 'ContentsCode', 'values')
-    value_texts = extract_variable_data(scb_response, 'ContentsCode', 'valueTexts')
+    value_texts = extract_variable_data(scb_response,
+            'ContentsCode', 'valueTexts')
+
     for item in zip(value_texts, codes):
         if 'riksdagsval' in item[0]:
             return item[1]
@@ -45,10 +81,13 @@ def retrieve_scb_voting_result_variables():
     return json.loads(response.text)
 
 
-def retrieve_scb_region_voting_data_by_type(riksdag_code):
+def retrieve_scb_voting_data_by_type(riksdag_code):
     scb_region_data_request = create_region_data_request(riksdag_code)
-    response = requests.post(VOTING_RESULTS_URL, scb_region_data_request)
-    return json.loads(response.text)
+    response = requests.post(VOTING_RESULTS_URL, scb_region_data_request,
+                             stream=True)
+    # Due to some bug, json cannot load this response due to a BOM header
+    # without explicit decoding.
+    return json.loads(response.raw.read().decode('utf-8-sig'))
 
 
 def create_region_data_request(contents_code):
@@ -71,7 +110,7 @@ def create_region_data_request(contents_code):
             {
                 "code": "Tid",
                 "selection": {
-                    "filter": "item",
+                    "filter": "all",
                     "values": ["*"]
                 }
             }
--- scbminer/voting_highscores/tests.py
+++ scbminer/voting_highscores/tests.py
@@ -1,81 +1,53 @@
 import unittest
 import services
-import json
-
-
-EXAMPLE_RESPONSE = json.loads("""{
-  \"title\": \"Valdeltagande efter region, tabellinnehåll och valår\",
-  \"variables\": [
-    {
-      \"code\": \"Region\",
-      \"text\": \"region\",
-      \"values\": [
-        \"00\",
-        \"0114\",
-        \"0115\"
-      ],
-      \"valueTexts\": [
-        \"Riket\",
-        \"Upplands Väsby\",
-        \"Vallentuna\"
-      ]
-    },
-    {
-      \"code\": \"ContentsCode\",
-      \"text\": \"tabellinnehåll\",
-      \"values\": [
-        \"ME0104B8\",
-        \"ME0104C5\",
-        \"ME0104C6\"
-      ],
-      \"valueTexts\": [
-        \"Valdeltagande i riksdagsval, procent\",
-        \"Valdeltagande i landstingsfullmäktigval, procent\",
-        \"Valdeltagande i kommunfullmäktigval, procent\"
-      ]
-    },
-    {
-      \"code\": \"Tid\",
-      \"text\": \"valår\",
-      \"values\": [
-        \"1973\",
-        \"1976\"
-      ],
-      \"valueTexts\": [
-        \"1973\",
-        \"1976\"
-      ],
-      \"time\": true
-    }
-  ]
-}""")
+from scb_test_data import *
 
 
 class TestServices(unittest.TestCase):
 
     def test_that_region_value_texts_can_be_extracted(self):
-        actual_value_texts = services.extract_value_texts(EXAMPLE_RESPONSE)
-        self.assertEquals(len(actual_value_texts), 3)
-        self.assertEquals(actual_value_texts[0], 'Riket')
-        self.assertEquals(actual_value_texts[1], 'Upplands Väsby')
-        self.assertEquals(actual_value_texts[2], 'Vallentuna')
+        actual_value_texts = services.extract_value_texts(VARIABLES_RESPONSE)
+        self.assertEqual(len(actual_value_texts), 3)
+        self.assertEqual(actual_value_texts[0], 'Riket')
+        self.assertEqual(actual_value_texts[1], 'Upplands Väsby')
+        self.assertEqual(actual_value_texts[2], 'Vallentuna')
 
     def test_that_region_values_can_be_extracted(self):
-        actual_value_texts = services.extract_values(EXAMPLE_RESPONSE)
-        self.assertEquals(len(actual_value_texts), 3)
-        self.assertEquals(actual_value_texts[0], '00')
-        self.assertEquals(actual_value_texts[1], '0114')
-        self.assertEquals(actual_value_texts[2], '0115')
+        actual_value_texts = services.extract_values(VARIABLES_RESPONSE)
+        self.assertEqual(len(actual_value_texts), 3)
+        self.assertEqual(actual_value_texts[0], '00')
+        self.assertEqual(actual_value_texts[1], '0114')
+        self.assertEqual(actual_value_texts[2], '0115')
 
     def test_that_region_code_map_can_be_extracted(self):
-        region_code_map = services.create_region_code_map(EXAMPLE_RESPONSE)
-        self.assertEquals(region_code_map['00'], 'Riket')
-        self.assertEquals(region_code_map['0114'], 'Upplands Väsby')
-        self.assertEquals(region_code_map['0115'], 'Vallentuna')
+        region_code_map = services.create_region_code_map(VARIABLES_RESPONSE)
+        self.assertEqual(region_code_map['00'], 'Riket')
+        self.assertEqual(region_code_map['0114'], 'Upplands Väsby')
+        self.assertEqual(region_code_map['0115'], 'Vallentuna')
 
     def test_that_riksdags_code_can_be_extracted_from_response(self):
-        actual_riksdag_code = services.extract_riksdag_code(EXAMPLE_RESPONSE)
+        actual_riksdag_code = services.extract_riksdag_code(VARIABLES_RESPONSE)
         self.assertEqual(actual_riksdag_code, 'ME0104B8')
+
+    def test_that_data_keys_can_be_extracted_from_voting_results(self):
+        actual_data_keys = services.extract_data_keys(DATA_RESPONSE);
+        self.assertEqual(len(actual_data_keys), 2)
+        self.assertEqual(actual_data_keys['Region'], 0)
+        self.assertEqual(actual_data_keys['Tid'], 1)
+
+    def test_that_voting_results_are_extracted_from_response(self):
+        code_map = services.create_region_code_map(VARIABLES_RESPONSE)
+        actual_results = services.extract_voting_results_by_year(DATA_RESPONSE,
+                                                                 code_map)
+
+        self.assertEqual(actual_results['1973'][0][0], 'Vallentuna')
+        self.assertEqual(actual_results['1973'][0][1], '93.3')
+        self.assertEqual(actual_results['1973'][1][0], 'Upplands Väsby')
+        self.assertEqual(actual_results['1973'][1][1], '91.4')
+        self.assertEqual(actual_results['2014'][0][0], 'Vallentuna')
+        self.assertEqual(actual_results['2014'][0][1], '89.3')
+        self.assertEqual(actual_results['2014'][1][0], 'Upplands Väsby')
+        self.assertEqual(actual_results['2014'][1][1], '84.1')
 
 
 if __name__ == '__main__':
